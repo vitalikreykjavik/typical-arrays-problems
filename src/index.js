15f6f6f5ca8127543c891222dabce6a51ded9f22
Vitali Serada <gomelska@gmail.com>
2020-03-22 16:32:38 +0300

    feat: add arrays problems function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return Math.min.apply(null, array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  if (array.length === 0) {
+    return 0;
+  }
+  return Math.max.apply(null, array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  if (array.length === 0) {
+    return 0;
+  }
+  let result = array.reduce(function(a,b) {
+    return a + b;
+  });
+  return result / array.length;
 }
